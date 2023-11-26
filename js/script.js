adcb11181a22a259a81bc8650ab11873d669f38e
soloninkov <103878111+soloninkov@users.noreply.github.com>
2023-11-26 16:44:13 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,34 @@
+//  slider------------------------------------
+const swiper = new Swiper(".slider-main-block", {
+  // Optional parameters
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".body-main-block__arrow.swiper-button-next",
+    prevEl: ".body-main-block__arrow.swiper-button-prev",
+  },
+});
+// edn-----------------------------------------
+// tabs----------------------------------------
+const tabNavItems = document.querySelectorAll(".tabs-deal__button");
+const tabItems = document.querySelectorAll(".item-tabs");
+document.addEventListener("click", function (e) {
+  const targetElement = e.target;
+  let currentActiveIndex = null;
+  let newActiveIndex = null;
+  if (targetElement.closest(".tabs-deal__button")) {
+    tabNavItems.forEach((tabNavItem, index) => {
+      if (tabNavItem.classList.contains("active")) {
+        currentActiveIndex = index;
+        tabNavItem.classList.remove("active");
+      }
+      if (tabNavItem === targetElement) {
+        newActiveIndex = index;
+      }
+    });
+    targetElement.classList.add("active");
+    tabItems[currentActiveIndex].classList.remove("active");
+    tabItems[newActiveIndex].classList.add("active");
+  }
+});
+// end-------------------------------------
